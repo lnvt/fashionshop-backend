3b0d27aadf7fab280b0ce120928be20d0441bd23
ThangLe <ThangLe@DESKTOP-TA83UJ7>
2021-10-24 12:44:45 +0700

    Refactor code and structure. Done Backend.

--- src/main/java/com/fashionshop/server/controller/ImageController.java
+++ src/main/java/com/fashionshop/server/controller/ImageController.java
@@ -1,54 +1,54 @@
 package com.fashionshop.server.controller;
 
 
+import com.fashionshop.server.models.AccountModel;
 import com.fashionshop.server.models.ImageModel;
+import com.fashionshop.server.repositories.IImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.Optional;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RequestMapping("/api/image")
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class ImageController {
 
-//    @Autowired
-//    private IImageService imageService;
-//
-//    @GetMapping
-//    public ResponseEntity<Iterable<ImageModel>> getAllImage() {
-//        return new ResponseEntity<>(imageService.findAll(), HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<ImageModel> getImage(@PathVariable Long id) {
-//        Optional<ImageModel> imageModelOptional = imageService.findById(id);
-//        return imageModelOptional.map(image -> new ResponseEntity<>(image, HttpStatus.OK))
-//                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<ImageModel> createImage(@RequestBody ImageModel image) {
-//        return new ResponseEntity<>(imageService.save(image), HttpStatus.OK);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<ImageModel> updateImage(@PathVariable Long id, @RequestBody ImageModel image) {
-//        Optional<ImageModel> imageModelOptional = imageService.findById(id);
-//        return imageModelOptional.map(item -> {
-//            image.setImageId(item.getImageId());
-//            return new ResponseEntity<>(imageService.save(item), HttpStatus.OK);
-//        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<ImageModel> deleteImage(@PathVariable Long id) {
-//        Optional<ImageModel> imageModelOptional = imageService.findById(id);
-//        return imageModelOptional.map(item -> {
-//            imageService.remove(id);
-//            return new ResponseEntity<>(item, HttpStatus.OK);
-//        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
+
+    @Autowired
+    private IImageRepository imageRepository;
+
+    @GetMapping
+    public List<ImageModel> getAllImage() {
+        return imageRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ImageModel getImage(@PathVariable Long id) {
+        return imageRepository.findById(id).get();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createImage(@RequestBody ImageModel image) {
+        ImageModel imageCreate = imageRepository.save(image);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(imageCreate.getImageId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ImageModel> updateImage(@PathVariable Long id, @RequestBody ImageModel image) {
+        ImageModel imageModel = imageRepository.save(image);
+        return new ResponseEntity<ImageModel>(image, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteImage(@PathVariable Long id) {
+        imageRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/com/fashionshop/server/controller/PostController.java
+++ src/main/java/com/fashionshop/server/controller/PostController.java
@@ -1,54 +1,52 @@
 package com.fashionshop.server.controller;
 
 
+import com.fashionshop.server.models.AccountModel;
 import com.fashionshop.server.models.PostModel;
+import com.fashionshop.server.repositories.IPostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.Optional;
+import java.net.URI;
+import java.util.List;
 
 @RequestMapping("/api/post")
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class PostController {
 
-//    @Autowired
-//    private IPostService postService;
-//
-//    @GetMapping
-//    public ResponseEntity<Iterable<PostModel>> getAllPost() {
-//        return new ResponseEntity<>(postService.findAll(), HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<PostModel> getPost(@PathVariable Long id) {
-//        Optional<PostModel> postModelOptional = postService.findById(id);
-//        return postModelOptional.map(post -> new ResponseEntity<>(post, HttpStatus.OK))
-//                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<PostModel> createPost(@RequestBody PostModel post) {
-//        return new ResponseEntity<>(postService.save(post), HttpStatus.OK);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<PostModel> updatePost(@PathVariable Long id, @RequestBody PostModel post) {
-//        Optional<PostModel> postModelOptional = postService.findById(id);
-//        return postModelOptional.map(item -> {
-//            post.setPostId(item.getPostId());
-//            return new ResponseEntity<>(postService.save(item), HttpStatus.OK);
-//        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<PostModel> deletePost(@PathVariable Long id) {
-//        Optional<PostModel> postModelOptional = postService.findById(id);
-//        return postModelOptional.map(item -> {
-//            postService.remove(id);
-//            return new ResponseEntity<>(item, HttpStatus.OK);
-//        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
+    @Autowired
+    private IPostRepository postRepository;
+
+    @GetMapping
+    public List<PostModel> getAllPost() {
+        return postRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public PostModel getPost(@PathVariable Long id) {
+        return postRepository.findById(id).get();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createPost(@RequestBody PostModel post) {
+        PostModel postCreate = postRepository.save(post);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(postCreate.getPostId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostModel> updatePost(@PathVariable Long id, @RequestBody PostModel post) {
+        PostModel postModelUpdate = postRepository.save(post);
+        return new ResponseEntity<PostModel>(post, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+        postRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
